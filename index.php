1f3bd7c2e399fd760a069efbaba3ffcd3e1a92a3
Code Genius - Studio <159528992+CodeGenius-Studio@users.noreply.github.com>
2024-02-23 00:02:50 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<?php 
-if($mysql->connect_error) {
-echo "Connection failed: ". $mysql ->connect_error;
-} else {
-echo "Connected successfully\n";
-}
+<?php
+$local_file = "uploads/" . $_FILES["fileToUpload"]["name"];
+$remote_url = "https://codegenius-studio.github.io/SayarKyan.com/";
 
+if (copy($local_file, $remote_url)) {
+    echo "File uploaded to remote URL successfully.";
+} else {
+    echo "Failed to upload file to remote URL.";
+}
 ?>
