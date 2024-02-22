43c2e539cf767f553e8bcf00d1f40827b382ae15
Code Genius - Studio <159528992+CodeGenius-Studio@users.noreply.github.com>
2024-02-23 00:05:50 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 <?php
-$local_file = "uploads/" . $_FILES["fileToUpload"]["name"];
-$remote_url = "https://codegenius-studio.github.io/SayarKyan.com/";
+	
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+				
+	function get_data() {
+		$name = $_POST['name'];
+		$file_name='StudentsData'. '.json';
 
-if (copy($local_file, $remote_url)) {
-    echo "File uploaded to remote URL successfully.";
-} else {
-    echo "Failed to upload file to remote URL.";
+		if(file_exists("$file_name")) {
+			$current_data=file_get_contents("$file_name");
+			$array_data=json_decode($current_data, true);
+							
+			$extra=array(
+				'Name' => $_POST['name'],
+				'Branch' => $_POST['branch'],
+				'Year' => $_POST['year'],
+			);
+			$array_data[]=$extra;
+			echo "file exist<br/>";
+			return json_encode($array_data);
+		}
+		else {
+			$datae=array();
+			$datae[]=array(
+				'Name' => $_POST['name'],
+				'Branch' => $_POST['branch'],
+				'Year' => $_POST['year'],
+			);
+			echo "file not exist<br/>";
+			return json_encode($datae);
+		}
+	}
+
+	$file_name='StudentsData'. '.json';
+	
+	if(file_put_contents("$file_name", get_data())) {
+		echo 'success';
+	}				
+	else {
+		echo 'There is some error';				
+	}
 }
+	
 ?>
